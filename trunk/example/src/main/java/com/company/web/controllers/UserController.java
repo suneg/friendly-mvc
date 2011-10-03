5febcb2cee3ecaa8ab0de2b3e1c9c5256fcee059
sune.gynthersen <sune.gynthersen@b4276792-cae8-4f65-fbb9-9542dd8e4213>
2011-10-03 19:16:41 +0000

    Added better examples in the UserController

--- trunk/example/src/main/java/com/company/web/controllers/UserController.java
+++ trunk/example/src/main/java/com/company/web/controllers/UserController.java
@@ -1,26 +1,36 @@
 package com.company.web.controllers;
 
+import com.company.web.models.User;
 import dk.bestbrains.friendly.BaseController;
 import dk.bestbrains.friendly.ParameterOrder;
 import dk.bestbrains.friendly.results.JsonResult;
+import dk.bestbrains.friendly.results.XmlResult;
 
 public class UserController extends BaseController {
 
-    public void all() {
-        
-    }
-
     public void index() {
-        
+    
     }
 
     @ParameterOrder("id")
-    public JsonResult details_json(String id) {
-        return toJson("hello world details with id " + id);
+    public JsonResult details_json(int id) {
+        User user = loadUser(id);
+        return toJson(user);
+    }
+
+    @ParameterOrder("id")
+    public XmlResult details_xml(int id) {
+        User user = loadUser(id);
+        return toXml(user);
     }
 
     @ParameterOrder("id")
     public void details(int id) {
-        View.put("id", id);
+        User user = loadUser(id);
+        View.put("user", user);
+    }
+
+    private User loadUser(int id) {
+        return User.construct(id, "Sune Gynthersen", "BestBrains", 30);
     }
 }
